767edbb50d8aa7dd1c89de5c0b16e7de7e267905
Meghan <meghan.e.mccue@gmail.com>
2020-01-17 13:33:27 +0000

    Duplicates removed. Removed 'user' from database connection

--- lib/bookmark.rb
+++ lib/bookmark.rb
@@ -13,17 +13,17 @@ class Bookmark
   end
 
   def self.create(title:, url:)
-    connection = PG.connect :dbname => environment, :user => 'bsas'
+    connection = PG.connect :dbname => environment
     connection.exec("INSERT INTO bookmarks (title, url) VALUES ('#{title}', '#{url}')")
   end
 
   def self.delete(id:)
-    connection = PG.connect :dbname => environment, :user => 'bsas'
+    connection = PG.connect :dbname => environment
     connection.exec("DELETE FROM bookmarks WHERE id = #{id}")
   end
 
   def self.all
-    connection = PG.connect :dbname => environment, :user => 'bsas'
+    connection = PG.connect :dbname => environment
     result = connection.exec("SELECT * FROM bookmarks")
     # result.map { |row| Bookmark.new(id: row['id'], title: row['title'], url: row['url']) }
     result.map { |row| Bookmark.new(row) }
--- spec/database_helpers.rb
+++ spec/database_helpers.rb
@@ -1,7 +1,7 @@
 require 'pg'
 
 def setup_test_database
-  connection = PG.connect :dbname => 'bookmark_manager_test', :user => 'bsas'
+  connection = PG.connect :dbname => 'bookmark_manager_test'
   connection.exec('TRUNCATE TABLE bookmarks')
   connection.exec("INSERT INTO bookmarks (title, url) VALUES ('Google', 'http://www.google.com')")
 end
